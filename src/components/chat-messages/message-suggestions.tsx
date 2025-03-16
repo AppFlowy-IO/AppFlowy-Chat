d85f4834b32e3c6b79d6f5d5de1725102e31b747
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2025-03-16 11:58:30 +0800

    chore: use line wrap instead of tooltip to view suggested question

--- src/components/chat-messages/message-suggestions.tsx
+++ src/components/chat-messages/message-suggestions.tsx
@@ -1,7 +1,6 @@
 import { useChatContext } from '@/chat/context';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { useTranslation } from '@/i18n';
 import { MESSAGE_VARIANTS } from '@/lib/animations';
 import { useMessagesHandlerContext } from '@/provider/messages-handler-provider';
@@ -46,34 +45,27 @@ export function MessageSuggestions({ suggestions }: MessageSuggestionsProps) {
       className={'flex flex-col gap-4 w-full overflow-hidden mr-auto'}
     >
       <Label className={'opacity-60'}>{t('suggestion.title')}</Label>
-      <TooltipProvider>
-        <div className={'flex gap-2 flex-col items-start w-full overflow-hidden'}>
-          {suggestions.items.length === 0 ? (
-            <EmptySuggestions />
-          ) : (
-            suggestions.items.map((suggestion, index) => (
-              <Tooltip disableHoverableContent={true}>
-                <TooltipTrigger asChild>
-                  <Button
-                    key={index}
-                    className={'w-full justify-start overflow-hidden'}
-                    onClick={() => handleClick(suggestion.content)}
-                    startIcon={
-                      <span className={'text-primary'}>
-                        <ChatIcon />
-                      </span>
-                    }
-                    variant={'ghost'}
-                  >
-                    <span className={'truncate !text-foreground/85'}>{suggestion.content}</span>
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>{suggestion.content}</TooltipContent>
-              </Tooltip>
-            ))
-          )}
-        </div>
-      </TooltipProvider>
+      <div className={'flex gap-2 flex-col items-start w-full overflow-hidden'}>
+        {suggestions.items.length === 0 ? (
+          <EmptySuggestions />
+        ) : (
+          suggestions.items.map((suggestion, index) => (
+            <Button
+              key={index}
+              className={'w-full justify-start overflow-hidden h-fit p-2'}
+              onClick={() => handleClick(suggestion.content)}
+              startIcon={
+                <span className={'text-primary'}>
+                  <ChatIcon />
+                </span>
+              }
+              variant={'ghost'}
+            >
+              <span className={'text-wrap !text-foreground/85 text-start'}>{suggestion.content}</span>
+            </Button>
+          ))
+        )}
+      </div>
     </motion.div>
   );
 }
